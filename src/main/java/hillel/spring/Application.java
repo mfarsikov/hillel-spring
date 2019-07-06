d4a52f47cacc61bd2aebf13b40743d22479b977c
max <mf@conciliolabs.com>
2019-07-06 13:09:23 +0300

    hello rest

--- src/main/java/hillel/spring/Application.java
+++ src/main/java/hillel/spring/Application.java
@@ -2,6 +2,7 @@ package hillel.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +11,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public String knightName(){
+        return "Keehot";
+    }
 }
