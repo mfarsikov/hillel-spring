d5392dffca9c4c8f0fb3f0aad761b567a00ad379
max <mf@conciliolabs.com>
2019-07-06 10:38:03 +0300

    dummy test

--- src/test/java/hillel/spring/AppTest.java
+++ src/test/java/hillel/spring/AppTest.java
@@ -11,4 +11,9 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test
+    public void dummyTest() throws Exception {
+        assertTrue(true);
+    }
 }
