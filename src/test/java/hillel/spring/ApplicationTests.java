ab6b4c11e6d0dcb4dc7d29fd3ed6f490e363e91b
max <mf@conciliolabs.com>
2019-08-10 10:59:05 +0300

    testcontainers

--- src/test/java/hillel/spring/ApplicationTests.java
+++ src/test/java/hillel/spring/ApplicationTests.java
@@ -2,11 +2,13 @@ package hillel.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@TestRunner
 public class ApplicationTests {
 
 	@Test
---
+++ src/test/java/hillel/spring/TestRunner.java
@@ -0,0 +1,17 @@
+package hillel.spring;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public @interface TestRunner {
+}
