0a9b7e50e332b12b40b251efb69aaa6a08082e8d
max <mf@conciliolabs.com>
2019-09-07 10:54:20 +0300

    config  validation

--- src/main/java/hillel/spring/petclinic/WorkingHoursConfig.java
+++ src/main/java/hillel/spring/petclinic/WorkingHoursConfig.java
@@ -1,13 +1,19 @@
 package hillel.spring.petclinic;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @Configuration
 @ConfigurationProperties("pet-clinic.working-hours")
+@Validated
 public class WorkingHoursConfig {
+    @NotBlank
     private String start;
+    @NotBlank
     private String end;
 }
